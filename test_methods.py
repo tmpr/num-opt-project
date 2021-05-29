2e5f76a3719fc83cddef9cf71a9b7d38a19f4155
Alexander <alexandertemper@protonmail.com>
2021-05-29 11:42:55 +0200

    🐛 FIX: add `exist_ok = True`

--- test_methods.py
+++ test_methods.py
@@ -13,18 +13,17 @@ from numbers import Number
 def test_delete_reports():
     reports = Path("reports")
     reports.mkdir(exist_ok=True)
-    (reports/'success').mkdir()
-    (reports/'fail').mkdir()
+    (reports/'success').mkdir(exist_ok=True)
+    (reports/'fail').mkdir(exist_ok=True)
     rmtree(reports/'success')
     rmtree(reports/'fail')
-    (reports/'success').mkdir()
-    (reports/'fail').mkdir()
+    (reports/'success').mkdir(exist_ok=True)
+    (reports/'fail').mkdir(exist_ok=True)
 
 
-
-@pytest.fixture(params=[Newton, 
-                        QuasiNewton, 
-                        SteepestDescent, 
+@pytest.fixture(params=[Newton,
+                        QuasiNewton,
+                        SteepestDescent,
                         ConjugateGradient
                         ])
 def method(request):
@@ -33,8 +32,9 @@ def method(request):
 
 @pytest.mark.parametrize('function', UNI_BI_VARIATE + MULTIVARIATE)
 def test_methods(function: Function, method: Minimizer):
-    x_0 = np.ones(shape=function.minimizers[0].shape) * 3 if not function.is_univariate else 5
+    x_0 = np.ones(
+        shape=function.minimizers[0].shape) * 3 if not function.is_univariate else 3
     minimizer = method(function=function, x_0=x_0)
     minimizer.minimize()
-    
+
     assert minimizer.has_converged()
