5e63c2993cb7da74d0447c980b677aedafe551c2
Alexander <alexandertemper@protonmail.com>
2021-05-29 10:59:10 +0200

    👌 IMPROVE: Remove cosh example

--- examples.py
+++ examples.py
@@ -1,6 +1,6 @@
 import numpy as np
 from function_object import Function, MatrixFunction, MatrixGrad, MatrixConstant
-from math import sin, cos, sinh, cosh
+from math import sin, cos
 
 np.random.seed(42)
 
@@ -30,10 +30,10 @@ UNI_BI_VARIATE = [
         minimizers= [-0.59198478817]
     ),
     Function(
-        f = lambda x: cosh(x) + x**3,
-        gradient = lambda x: sinh(x) + 3*x**2,
-        hessian = lambda x: cosh(x) + 6*x,
-        minimizers= [0, -5.01777749355]
+        f = lambda x: 5*cos(x) + x**4,
+        gradient = lambda x: -5*sin(x) + 4*x**3,
+        hessian = lambda x: -5*cos(x) + 12*x**2,
+        minimizers= [-1.0215881550565, 1.0215881550565]
     ),
 ]
 
